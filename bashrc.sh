2c7c2641b85d235c0d4cc9d8bfbfe80fa033c5ff
Madhav Manoj <madhavmanoj117@gmail.com>
2018-12-05 15:58:15 +0530

    added custom bash config & updated installer

---
+++ bashrc.sh
@@ -0,0 +1,28 @@
+############ ---------------------------------------------------------------------- ###############
+
+# Custom edits to Bash Profile
+
+### Custom aliases
+
+# Tmux
+alias tat="tmux a -t"
+alias tns="tmux new -s"
+alias tls="tmux ls"
+
+# System
+alias l="ls -larth"
+
+# Gcloud
+alias gcp_switch="gcloud config configurations activate"
+
+# Git
+alias gpull="git pull -p"
+alias gcom="git commit"
+alias gpush="git stash"
+alias gpop="git stash pop"
+alias gba="git branch -a"
+alias gcb="git checkout"
+alias gcbn="git checkout -b"
+alias gs="git status"
+
+
--- install_conf.sh
+++ install_conf.sh
@@ -6,3 +6,10 @@ cp ./vimrc ~/.vimrc
 # Sourcing tmux file, in case tmux is running
 tmux source-file ~/.tmux.conf
 echo "TMUX Configuration reloaded"
+
+# Applying custom bashrc config
+cat bashrc.sh > ~/.bashrc
+
+# Fix vimrc file
+# TODO: add code to modify VIMRC file as necessary
+# ie, to ensure the version of vim, in the runtimepath is set correctly
