446ef96c1b25de74c05169ff327ed65ccb80c96e
Madhav Manoj <madhavmanoj117@gmail.com>
2026-09-06 14:55:55 +0200

    chore: update shell and bar configs
    - Move sketchybar calendar item to left side
    - Enable tmux focus and extended keys for coding agents
    - Set default zsh editor to nvim

--- sketchybar/items/calendar.sh
+++ sketchybar/items/calendar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sketchybar --add item calendar right \
+sketchybar --add item calendar left \
            --set calendar icon=􀉉 \
            update_freq=30 \
            script="$PLUGIN_DIR/calendar.sh"
